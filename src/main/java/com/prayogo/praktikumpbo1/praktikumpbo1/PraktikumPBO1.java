12c754e91e88d9f3143579d8518dffa9717cc18d
PRAYOGO <PRAYOGO@Iphone-Xr>
2022-12-22 21:44:07 +0700

    PraktikumPBO1

---
+++ src/main/java/com/prayogo/praktikumpbo1/praktikumpbo1/PraktikumPBO1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.prayogo.praktikumpbo1.praktikumpbo1;
+
+/**
+ *
+ * @author PRAYOGO
+ */
+public class PraktikumPBO1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Prayogo Sindurojo Kusumojati");
+        System.out.println("NIM : 2215103009");
+    }
+}
